3071cec3a03f359d16021eb3830132021513dfc5
adebayo segun <adebayosegun174@gmail.com>
2023-08-12 15:15:25 +0100

    refactored code to use both cobra and viper to read file froma file

--- cmd/root.go
+++ cmd/root.go
@@ -1,59 +1,89 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 Adebayosegun174@gmail.com
 */
 package cmd
 
 import (
-	"os"
+	"log"
+	"strconv"
 
 	"github.com/ipsegs/sshpodlog/pkg"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "sshpodlog",
-	Short: "A tool to manage Kubernetes pod logs",
-	Long:  `A CLI tool designed for efficient management and retrieval of logs from Kubernetes pods. This tool operates by utilizing SSH connections to establish access through a dedicated jump server, also known as a jump host or Bastion host. This jump server acts as an intermediary node to ensure secure access to the target environment. The jump server is equipped with a preinstalled instance of kubectl, the Kubernetes command-line tool. This strategic setup empowers the CLI to simplify the process of fetching logs from Kubernetes clusters Through the utilization of SSH connections and the jump server, the utility offers a secure gateway into the Kubernetes environment. By leveraging the integrated "kubectl", users can seamlessly extract logs from various pods within the cluster. This approach streamlines the process of log retrieval, ultimately enhancing the overall management and troubleshooting capabilities within the Kubernetes ecosystem.`,
-	Run:   defaultFunction,
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
-}
-
-var (
+var flags struct {
 	Server        string
 	Username      string
 	KctlCtxSwitch string
 	PrivateKey    string
 	Port          int
-)
-
-func init() {
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVarP(&Server, "server", "s", "", "usage")
-	rootCmd.PersistentFlags().StringVarP(&Username, "username", "u", "", "usage")
-	rootCmd.PersistentFlags().StringVarP(&KctlCtxSwitch, "cluster", "c", "current", "usage")
-	rootCmd.PersistentFlags().StringVarP(&PrivateKey, "key", "k", "", "usage")
-	rootCmd.PersistentFlags().IntVarP(&Port, "port", "p", 22, "usage")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	FromFile   string
 }
 
-//The rootCMD that will be run which is the default function being run if no subcommand is added.
-func defaultFunction(cmd *cobra.Command, args []string) {
-	pkg.Sshpodlog(Server, Username, KctlCtxSwitch, PrivateKey, Port)
+var rootCmd = &cobra.Command{
+	Use:   "sshpodlog",
+	Short: "A tool to manage Kubernetes pod logs",
+	Long:  `A CLI tool designed for efficient management and retrieval of logs from Kubernetes pods. ...`,
+	Run:   defaultFunction,
+}
 
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Global flags
+	rootCmd.PersistentFlags().StringVarP(&flags.Server, "server", "s", "", "SSH server address")
+	rootCmd.PersistentFlags().StringVarP(&flags.Username, "username", "u", "", "SSH username")
+	rootCmd.PersistentFlags().StringVarP(&flags.KctlCtxSwitch, "cluster", "c", "current", "kubectl context switch")
+	rootCmd.PersistentFlags().StringVarP(&flags.PrivateKey, "key", "k", "", "SSH private key path")
+	rootCmd.PersistentFlags().IntVarP(&flags.Port, "port", "p", 22, "SSH port")
+	rootCmd.PersistentFlags().StringVarP(&flags.FromFile, "from-file", "f", "default", "Configuration properties file")
+}
+
+func initConfig() {
+	if flags.FromFile != "" {
+		viper.SetConfigFile(flags.FromFile)
+		viper.ReadInConfig()
+	}
+}
+
+func defaultFunction(cmd *cobra.Command, args []string) {
+
+	if cmd.Flag("from-file").Changed {
+		flags.Server = viper.GetString("server")
+		flags.Username = viper.GetString("username")
+		flags.KctlCtxSwitch = viper.GetString("cluster")
+		flags.PrivateKey = viper.GetString("key")
+		flags.Port = viper.GetInt("port")
+	} else {
+		// Use the values from command-line flags if provided
+		if ServerFlag := cmd.Flag("server"); ServerFlag != nil && ServerFlag.Changed {
+			flags.Server = ServerFlag.Value.String()
+		}
+		if UsernameFlag := cmd.Flag("username"); UsernameFlag != nil && UsernameFlag.Changed {
+			flags.Username = UsernameFlag.Value.String()
+		}
+		if KctlCtxSwitchFlag := cmd.Flag("cluster"); KctlCtxSwitchFlag != nil && KctlCtxSwitchFlag.Changed {
+			flags.KctlCtxSwitch = KctlCtxSwitchFlag.Value.String()
+		}
+		if PrivateKeyFlag := cmd.Flag("key"); PrivateKeyFlag != nil && PrivateKeyFlag.Changed {
+			flags.PrivateKey = PrivateKeyFlag.Value.String()
+		}
+		if PortFlag := cmd.Flag("port"); PortFlag != nil && PortFlag.Changed {
+			port, err := strconv.Atoi(PortFlag.Value.String())
+			if err != nil {
+				log.Printf("Error parsing port: %v", err)
+			}
+			flags.Port = port
+		}
+	}
+
+	pkg.Sshpodlog(flags.Server, flags.Username, flags.KctlCtxSwitch, flags.PrivateKey, flags.Port)
 }
